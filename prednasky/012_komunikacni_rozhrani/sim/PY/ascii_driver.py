481fb8771a69b410e4d17e7ea9bdc2dd0d9dba00
brejcmic <brejchac@fel.cvut.cz>
2026-05-10 17:19:20 +0200

    Zkouska komunikace s ControlWeb

---
+++ prednasky/012_komunikacni_rozhrani/sim/PY/ascii_driver.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Filename: ascii?driver.py
+Author: Michal Brejcha
+Date: 2026-05-07
+Version: 1.0
+Description: Ukázka komunikace s prostředím ControlWeb
+"""
+
+import serial
+
+class SerialExample:
+    """
+    Objekt ukázkového příkladu sériové komunikace
+    """
+    def __init__(self):
+        """
+        Nastavení sériové komunikace
+        """
+        self.__dev = serial.Serial(port='/dev/serial0',
+                                   baudrate=115200,
+                                   parity=serial.PARITY_NONE,
+                                   stopbits=serial.STOPBITS_ONE,
+                                   timeout=2)
+        self.__bytes_cnt = 0
+
+    def __del__(self):
+        """
+        Uzavření komunikace
+        """
+        if self.__dev is not None and self.__dev.is_open:
+            self.__dev.close()
+
+    def transaction_call(self):
+        response = self.__dev.readline()
+        if len(response) > 0:
+            print("Přijata zpráva: {0}".format(response))
+
+
+if __name__ == '__main__':
+    obj = SerialExample()
+
+    while True:
+        obj.transaction_call()
